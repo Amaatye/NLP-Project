faafcb3af94ba27649b0cf98e78b544facc973b9
Amaatye <68722268+Amaatye@users.noreply.github.com>
2020-11-06 14:58:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+
+from nltk.tokenize import sent_tokenize
+import pickle
+from textblob import TextBlob
+
+l1 = []
+business = {}
+
+for i in range(1, 417):
+    if i<10:
+        k="0"+"0"+str(i)
+    elif i<100:
+        k="0"+str(i)
+    else:
+        k=str(i)
+    l1.append(k)
+
+l = []
+count = 0
+count1 = 0
+count2 = 0
+score = 0
+
+for i in l1:
+    with open("bbc/politics/"+str(i)+".txt") as file:
+        for read in file:
+            a=sent_tokenize(read)
+            l.append(a)
+            for i in a:
+                analysis = TextBlob(i)
+                print(i, analysis.sentiment.polarity)
+                score = score+analysis.sentiment.polarity
+        print(score)
+        l = [j.replace("\n", "") for i in l for j in i]
+        line = ""
+        for i in l:
+            line = line+" "+str(i)
+        if score > 0:
+            count = count+1
+            business[line] = 1
+        elif score < 0:
+            count1 = count1+1
+            business[line] = -1
+        else:
+            count2 = count2+1
+            business[line] = 0
+        score = 0
+        line = ""
+        l = []
+
+print(count, count1, count2)
+with open("politic.pickle", 'wb') as dic:
+    pickle.dump(business, dic, protocol=pickle.HIGHEST_PROTOCOL)
+
